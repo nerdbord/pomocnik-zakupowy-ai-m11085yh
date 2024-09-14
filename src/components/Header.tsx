1a564ea3d0e68e2d069c07a628f0cc6a627ef49f
AniaPiWo <snowflake@wp.pl>
2024-09-14 16:12:14 +0200

    shopbotek

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GoPlus } from "react-icons/go";
+import { RiRobot2Line } from "react-icons/ri";
 
 type Props = {
   onAddClick: () => void;
@@ -8,7 +9,11 @@ type Props = {
 export const Header = ({ onAddClick }: Props) => {
   return (
     <div className="flex justify-between pb-2">
-      <h1 className="text-xl not-italic font-semibold leading-7">AI Zakupy</h1>
+      <div className="flex items-center gap-2">
+        <RiRobot2Line className="text-2xl mb-1" />
+        <h1 className="text-xl not-italic font-semibold">ShopBotek</h1>
+      </div>
+
       <div
         className="tooltip tooltip-info tooltip-left"
         data-tip="Create a new chat"
