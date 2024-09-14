767ad70b5d4d73e274ba88e6802f4695cd4c06ae
Mateusz <matmatwiej@gmail.com>
2024-09-14 17:19:25 +0200

    Feat/implement loader logic (#13)
    * feat: display loader properly
    
    * fix: remove log

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -7,11 +7,18 @@ import { RiArrowRightSLine } from "react-icons/ri";
 import { Pill } from "@/components/Pill";
 
 export default function Home() {
-  const { messages, handleInputChange, handleSubmit, input } = useChat();
+  const { messages, handleInputChange, handleSubmit, input, isLoading } =
+    useChat();
   const chatParent = useRef<HTMLUListElement | null>(null);
   const inputRef = useRef<HTMLTextAreaElement | null>(null);
+  const [hasStartedTyping, setHasStartedTyping] = useState(false);
 
-  const [loading, setLoading] = useState(true);
+  const messagesWithSystem: Message[] = [
+    { role: "system", content: "How can I help you?", id: "1" },
+    ...messages,
+  ];
+
+  const loading = isLoading && !hasStartedTyping;
 
   const handleAddClick = () => {
     console.log("header button clicked");
@@ -39,10 +46,17 @@ export default function Home() {
     }
   };
 
-  const messagesWithSystem: Message[] = [
-    { role: "system", content: "How can I help you?", id: "1" },
-    ...messages,
-  ];
+  useEffect(() => {
+    if (isLoading) {
+      setHasStartedTyping(false);
+    }
+
+    const lastMessage = messagesWithSystem[messagesWithSystem.length - 1];
+
+    if (lastMessage && lastMessage.role === "assistant") {
+      setHasStartedTyping(true);
+    }
+  }, [messagesWithSystem, isLoading]);
 
   return (
     <div className="flex flex-col h-full justify-between p-4">
@@ -54,7 +68,7 @@ export default function Home() {
         ref={chatParent}
         className="flex-grow overflow-y-auto mb-4 no-scrollbar p-2"
       >
-        {messagesWithSystem.map((m) => (
+        {messagesWithSystem.map(m => (
           <Fragment key={m.id}>
             {m.role === "user" ? (
               <ChatMessage position="end" message={m.content} />
@@ -63,6 +77,10 @@ export default function Home() {
             )}
           </Fragment>
         ))}
+
+        {loading && (
+          <ChatMessage position="start" message="" loading={loading} />
+        )}
       </ul>
 
       <div className="flex gap-4 my-4 flex-wrap flex-shrink-0">
