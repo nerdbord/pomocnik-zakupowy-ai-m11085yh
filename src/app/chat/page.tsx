4aec0288ad7324a91621a66b1b326e0ed2fc219f
AniaPiWo <snowflake@wp.pl>
2024-09-14 16:03:23 +0200

    scroll hotfix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -29,30 +29,38 @@ export default function Home() {
 
   return (
     <div className="flex flex-col h-full justify-between p-4">
-      <div>
+      {/* Fixed Header */}
+      <div className="flex-shrink-0">
         <Header onAddClick={handleAddClick} />
-
-        <ul
-          ref={chatParent}
-          className="flex-grow overflow-y-auto mb-4 no-scrollbar"
-        >
-          {messagesWithSystem.map((m) => (
-            <Fragment key={m.id}>
-              {m.role === "user" ? (
-                <ChatMessage position="end" message={m.content} />
-              ) : (
-                <ChatMessage
-                  position="start"
-                  message={m.content}
-                  /*          loading={loading && m.role !== "system"} */
-                />
-              )}
-            </Fragment>
-          ))}
-        </ul>
       </div>
 
-      <form className="flex items-center" onSubmit={handleSubmit}>
+      {/* Chat area */}
+      <ul
+        ref={chatParent}
+        className="flex-grow overflow-y-auto mb-4 no-scrollbar p-2"
+      >
+        {messagesWithSystem.map((m) => (
+          <Fragment key={m.id}>
+            {m.role === "user" ? (
+              <ChatMessage position="end" message={m.content} />
+            ) : (
+              <ChatMessage
+                position="start"
+                message={m.content}
+                /*          loading={loading && m.role !== "system"} */
+              />
+            )}
+            {/* Example message */}
+            <ChatMessage
+              position="end"
+              message="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate aperiam ab magnam voluptatum suscipit blanditiis nisi expedita iste reprehenderit soluta nam officiis, est aut praesentium voluptates, nihil, facere minima saepe accusamus! Eligendi, provident nulla possimus nihil eum aut enim sunt sint mollitia. Explicabo adipisci eligendi suscipit corrupti modi dolore quae!"
+            />
+          </Fragment>
+        ))}
+      </ul>
+
+      {/* Fixed Input Form */}
+      <form className="flex items-center flex-shrink-0" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter what you are looking for..."
