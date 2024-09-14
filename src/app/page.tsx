5a7c8ce9bf10cbf99362690ddd8be93db926da5b
AniaPiWo <snowflake@wp.pl>
2024-09-14 13:29:08 +0200

    feat: avatars, main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./components/Header";
 import { ChatMessage } from "./components/ChatMessage";
+import { RiArrowRightSLine } from "react-icons/ri";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -11,31 +12,34 @@ export default function Home() {
   };
 
   return (
-    <div className="h-[852px] w-[393px] border p-4">
-      <div>
-        <Header onAddClick={handleAddClick} />
-        <div>
-          <ChatMessage
-            position="start"
-            message="It's over Anakin, I have the high ground."
-            avatarUrl="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-          />
-          <ChatMessage
-            position="end"
-            message="You underestimate my power!"
-            avatarUrl="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-          />
-          <ChatMessage
-            position="start"
-            message="You underestimate my power!"
-            avatarUrl="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-            loading={loading}
-          />
-        </div>
+    <div className="flex flex-col justify-between h-[852px] w-[393px] border p-4 ">
+      <Header onAddClick={handleAddClick} />
+
+      <div className="flex-grow overflow-y-auto mb-4 no-scrollbar">
+        <ChatMessage
+          position="start"
+          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem adipisci, autem ab veniam error tempora eligendi repellendus a commodi nesciunt non consequatur nemo labore enim cumque molestiae accusantium aliquam quaerat vitae, fugit soluta accusamus sapiente. Modi tempora commodi error accusantium ex temporibus ea neque! Fuga vero doloribus pariatur molestiae consectetur."
+        />
+        <ChatMessage
+          position="end"
+          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem adipisci, autem ab veniam error tempora eligendi repellendus a commodi nesciunt non consequatur nemo labore enim cumque molestiae accusantium aliquam quaerat vitae, fugit soluta accusamus sapiente. Modi tempora commodi error accusantium ex temporibus ea neque! Fuga vero doloribus pariatur molestiae consectetur."
+        />
+        <ChatMessage
+          position="start"
+          message="You underestimate my power!"
+          loading={loading}
+        />
       </div>
 
-      <div>
-        <input type="text" />
+      <div className="flex items-center">
+        <input
+          type="text"
+          placeholder="Enter what you are looking for..."
+          className="input input-bordered w-full max-w-xs mr-2"
+        />
+        <button className="btn btn-ghost">
+          <RiArrowRightSLine size={28} />
+        </button>
       </div>
     </div>
   );
