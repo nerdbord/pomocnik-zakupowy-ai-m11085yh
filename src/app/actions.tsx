f625d4e1e04e83f857ba8dbb1052f1256ed111b7
SzDabrowski <szdabrowsky@gmail.com>
2024-09-14 17:33:44 +0200

    feat: created gpt survey for details of choosen product

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -9,6 +9,7 @@ export async function continueConversation(messages: CoreMessage[]) {
   const result = await streamText({
     model: openai("gpt-4-turbo"),
     messages,
+    system: "you are ai helper, speak in polish",
   });
   const data = { test: "hello" };
   const stream = createStreamableValue(result.textStream);
