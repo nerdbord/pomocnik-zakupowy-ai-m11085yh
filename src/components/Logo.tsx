5552f361efb6941d94105b1e8992fad136d424de
AniaPiWo <snowflake@wp.pl>
2024-09-15 11:48:17 +0200

    feat: products

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,5 +3,5 @@ import { RiRobot2Line } from "react-icons/ri";
 type Props = {};
 
 export const Logo = (props: Props) => {
-  return <RiRobot2Line className="text-3xl text-fuchsia-600" />;
+  return <RiRobot2Line className="text-xl text-violet-500" />;
 };
